cf0f5429afd951f3ac12b1cae2917c65b545a1e4
b-artem <artemb.dev@gmail.com>
2017-07-30 16:43:46 +0300

    remove pry from code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    binding.pry
+    # binding.pry
     request.env['omniauth.origin'] || root_path
   end
 end
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    binding.pry
+    # binding.pry
     return sign_in_and_redirect @user if @user.email
     flash[:warning] = "No email provided by facebook"
     redirect_to request.env['omniauth.origin'] || root_path
