775230f1f46e5b583ff3fe92924aec9b4c8efc4b
b-artem <artemb.dev@gmail.com>
2017-07-30 16:09:39 +0300

    fix csrf issue and providing email

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,9 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user
+    binding.pry
+    return sign_in_and_redirect @user if @user.email
+    flash[:warning] = "No email provided by facebook"
+    redirect_to request.env['omniauth.origin'] || root_path
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Figaro.require_keys("FACEBOOK_KEY", "FACEBOOK_SECRET")
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-            scope: 'public_profile', info_fields: 'id'
-end
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+#             scope: 'public_profile', info_fields: 'id'
+# end
