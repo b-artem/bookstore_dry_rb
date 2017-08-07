cf9632b916d96357089a23a869126bf4a4db3641
b-artem <artemb.dev@gmail.com>
2017-08-07 13:18:33 +0300

    add signup test

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -2,36 +2,37 @@ require 'rails_helper'
 require 'support/factory_girl'
 
 feature 'Login' do
+  let(:user) { create(:user) }
+  before do
+    visit new_user_session_path
+  end
+
   context 'when email and password are valid' do
     scenario 'logs user in' do
-      user = create(:user)
-      visit new_user_session_path
       within 'form#new_user' do
         fill_in 'email', with: user.email
         fill_in 'password', with: user.password
         click_button 'Log in'
       end
+      expect(page).to have_text 'Signed in successfully'
       expect(page).not_to have_content 'Enter Email'
       expect(page).not_to have_content 'Password'
       expect(page).not_to have_button 'Log in'
-      expect(page).to have_text 'Signed in successfully'
     end
   end
 
-  context 'when email or password are invalid' do
+  context 'when password is invalid' do
     scenario "doesn't log user in" do
-      user = create(:user)
-      visit new_user_session_path
       within 'form#new_user' do
         fill_in 'email', with: user.email
         fill_in 'password', with: (user.password + '1')
         click_button 'Log in'
       end
-      expect(page).to have_content 'Invalid Email or password'
+      expect(page).to have_text 'Invalid Email or password'
+      expect(page).not_to have_text 'Signed in successfully'
       expect(page).to have_content 'Enter Email'
       expect(page).to have_content 'Password'
       expect(page).to have_button 'Log in'
-      expect(page).not_to have_text 'Signed in successfully'
     end
   end
 end
