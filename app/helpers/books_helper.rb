e40632f142062a1a35c4fc2b378d50b9c1b92041
b-artem <artemb.dev@gmail.com>
2017-08-17 23:12:46 +0300

    latest on home page

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -2,4 +2,8 @@ module BooksHelper
   def best_sellers(quantity = 4)
     Book.order('price DESC').limit(quantity).decorate
   end
+
+  def latest(quantity = 3)
+    Book.order('created_at DESC').limit(quantity).decorate
+  end
 end
