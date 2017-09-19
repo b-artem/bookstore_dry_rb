d6763be29c8d51f2bf033a54142882dc911c8ecc
b-artem <artemb.dev@gmail.com>
2017-09-19 17:44:15 +0300

    start best sellers specs

---
+++ spec/features/home/best_sellers_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+require 'support/factory_girl'
+require 'support/devise'
+
+shared_examples 'best sellers' do
+  background do
+    3.times { create :book_mobile_development }
+    3.times { create :book_photo }
+    3.times { create :book_web_design }
+    3.times { create :book_web_development }
+    visit home_index_path
+  end
+
+  # scenario 'displays latest 3 books, added onto website' do
+  #   latest = Book.order('created_at DESC').limit(3)
+  #   within '.carousel-inner' do
+  #     expect(page).to have_content(latest.first.title)
+  #     expect(page).to have_content(latest.second.title)
+  #     expect(page).to have_content(latest.third.description[0..20])
+  #   end
+  # end
+  #
+  # scenario 'does not display latest 4th book, added onto website' do
+  #   within '.carousel' do
+  #     expect(page).not_to have_content(Book.order('created_at DESC')
+  #                                           .fourth.title)
+  #   end
+  # end
+
+  context 'when user clicks the View icon' do
+    scenario 'details of item are shown', js: true do
+      # bestseller_mobile_development = Book
+      # latest = Book.order('created_at DESC').limit(2)
+      # id = Book
+      within('#bestsellers') { click_link("book-view-") }
+      within '.carousel-inner > .item.active' do
+        expect(page).to have_content(latest.first.title)
+      end
+      page.find('a.right.carousel-control').click
+      within '.carousel-inner > .item.active' do
+        expect(page).to have_content(latest.last.title)
+      end
+    end
+  end
+
+  context 'when user clicks Buy now button' do
+    scenario 'adds chosen book to the cart', js: true do
+      within '.carousel-inner > .item.active' do
+        click_button('Buy Now')
+      end
+      wait_for_ajax
+      expect(Cart.first.line_items.first.book).to eq Book.order('created_at DESC').first
+    end
+  end
+end
+
+feature 'Home page' do
+  feature 'best sellers' do
+    context 'when user is a guest' do
+      it_behaves_like 'best sellers'
+    end
+
+    context 'when user is logged in' do
+      let(:user) { create(:user) }
+      background { sign_in user }
+      it_behaves_like 'best sellers'
+    end
+  end
+end
