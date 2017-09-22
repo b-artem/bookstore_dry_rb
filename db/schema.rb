fb3a6fa7b0f829645d0af141c67c58485b25bc8c
b-artem <artemb.dev@gmail.com>
2017-09-22 21:46:10 +0300

    after migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908201646) do
+ActiveRecord::Schema.define(version: 20170922071839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,14 @@ ActiveRecord::Schema.define(version: 20170908201646) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "coupons", force: :cascade do |t|
+    t.string "code"
+    t.decimal "discount", precision: 4, scale: 1
+    t.date "valid_until"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "line_items", force: :cascade do |t|
     t.bigint "book_id"
     t.bigint "cart_id"
@@ -102,6 +110,8 @@ ActiveRecord::Schema.define(version: 20170908201646) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "use_billing_address_as_shipping", default: false
+    t.bigint "coupon_id"
+    t.index ["coupon_id"], name: "index_orders_on_coupon_id"
     t.index ["number"], name: "index_orders_on_number"
     t.index ["shipping_method_id"], name: "index_orders_on_shipping_method_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
@@ -163,6 +173,7 @@ ActiveRecord::Schema.define(version: 20170908201646) do
   add_foreign_key "line_items", "books"
   add_foreign_key "line_items", "carts"
   add_foreign_key "line_items", "orders"
+  add_foreign_key "orders", "coupons"
   add_foreign_key "orders", "shipping_methods"
   add_foreign_key "orders", "users"
   add_foreign_key "reviews", "books"
