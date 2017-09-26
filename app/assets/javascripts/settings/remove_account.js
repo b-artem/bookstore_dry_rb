7878ff8c68be703dcb5223e8468707daf1cbd5a1
b-artem <artemb.dev@gmail.com>
2017-09-26 12:10:37 +0300

    delete user works now

--- app/assets/javascripts/settings/remove_account.js
+++ app/assets/javascripts/settings/remove_account.js
@@ -1,29 +1,7 @@
 $(document).on('turbolinks:load', function() {
   $("#remove-account-cb").change(function() {
+    $("#remove-account-btn").prop('disabled', !$("#remove-account-btn").prop('disabled'));
     $("#remove-account-btn").toggleClass('disabled');
-    $("#remove-account-btn").toggle('disabled');
-
-    // if ( $(this).prop('checked') == true ) {
-    //   $("#remove-account-btn").removeClass('disabled');
-    //   $("#remove-account-btn").prop('disabled', false);
-    // } else {
-    //   $("#remove-account-btn").addClass('disabled');
-    //   $("#remove-account-btn").prop('disabled', true);
-    // }
   });
   console.log('remove_account');
 });
-
-
-// $(document).on('turbolinks:load', function() {
-//   $("#remove-account-cb").change(function() {
-//     if ( $(this).prop('checked') == true ) {
-//       $("#remove-account-btn").removeClass('disabled');
-//       $("#remove-account-btn").prop('disabled', false);
-//     } else {
-//       $("#remove-account-btn").addClass('disabled');
-//       $("#remove-account-btn").prop('disabled', true);
-//     }
-//   });
-//   console.log('remove_account');
-// });
