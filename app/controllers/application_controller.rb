11c2ec996573054b06e3ea3efbc20352e069f162
b-artem <artemb.dev@gmail.com>
2017-09-29 17:02:46 +0300

    double action trouble

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale_from_params
   before_action :store_user_location, if: :storable_location?
+  before_action :store_back_path, if: :storable_location?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   before_action :set_cart
@@ -52,5 +53,9 @@ class ApplicationController < ActionController::Base
 
     def store_user_location
       store_location_for(:user, request.fullpath)
+    end
+
+    def store_back_path
+      session[:back_path] = request.referer
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def back_path
+    session[:back_path] || books_path
+  end
 end
