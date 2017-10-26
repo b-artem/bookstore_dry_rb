d9858053f050b1516ba881fbd19ef727ab84f3cf
b-artem <artemb.dev@gmail.com>
2017-10-26 17:56:18 +0300

    refactor checkout_controller step 1

--- app/controllers/orders/checkout_controller.rb
+++ app/controllers/orders/checkout_controller.rb
@@ -12,23 +12,11 @@ class Orders::CheckoutController < ApplicationController
     @current_order = current_order
     case step
     when :address
-      @order = Forms::OrderForm.from_model(current_order)
-      billing_model = current_order.billing_address || current_user.billing_address
-      shipping_model = current_order.shipping_address || current_user.shipping_address
-      @order.billing_address = Forms::BillingAddressForm.from_model(billing_model)
-      @order.shipping_address = Forms::ShippingAddressForm.from_model(shipping_model)
+      load_addresses
     when :delivery
-      @order = current_order
-      @order.shipping_method ||= ShippingMethod.order(:price).first
-      @shipping_methods = ShippingMethod.order('price ASC')
-    when :payment
-      @order = current_order
-      @payment = Forms::PaymentForm.new
-      get_payment_data
-    when :confirm
-      @order = current_order
-      @payment = Forms::PaymentForm.new
-      get_payment_data
+      load_delivery
+    when :payment, :confirm
+      load_payment
     when :complete
       current_order.pay if current_order.may_pay?
       @order = current_order
@@ -40,26 +28,12 @@ class Orders::CheckoutController < ApplicationController
     @current_order = current_order
     case step
     when :address
-      if params[:order][:use_billing_address_as_shipping] == 'true'
-        use_billing = true
-      else
-        use_billing = false
-      end
-      @order = Forms::OrderForm.from_params(params[:order], id: current_order.id)
-              .with_context(use_billing_address_as_shipping: use_billing)
-      @order.billing_address.order_id = current_order.id
-      @order.shipping_address.order_id = current_order.id unless use_billing
-      render_next_step @order
-
+      save_addresses
     when :delivery then
       current_order.update_attributes(shipping_method_id: params[:order][:shipping_method_id])
-      @order = current_order
-      render_next_step @order
+      render_next_step current_order
     when :payment
-      @order = current_order
-      @payment = Forms::PaymentForm.from_params(params[:payment])
-      set_payment_data
-      render_next_step @payment
+      process_payment
     when :confirm
       @order = current_order
       render_wizard @order
@@ -70,14 +44,51 @@ class Orders::CheckoutController < ApplicationController
 
   private
 
-    def render_next_step(form)
-      if form.valid?
-        form.save
-        return redirect_to next_wizard_path unless params[:next_step] == 'confirm'
-        redirect_to wizard_path(:confirm)
+    def load_addresses
+      @order = Forms::OrderForm.from_model(current_order)
+      billing_model = current_order.billing_address || current_user.billing_address
+      shipping_model = current_order.shipping_address || current_user.shipping_address
+      @order.billing_address = Forms::BillingAddressForm.from_model(billing_model)
+      @order.shipping_address = Forms::ShippingAddressForm.from_model(shipping_model)
+    end
+
+    def save_addresses
+      if params[:order][:use_billing_address_as_shipping] == 'true'
+        use_billing = true
       else
-        render_wizard form
+        use_billing = false
       end
+      @order = Forms::OrderForm.from_params(params[:order], id: current_order.id)
+              .with_context(use_billing_address_as_shipping: use_billing)
+      @order.billing_address.order_id = current_order.id
+      @order.shipping_address.order_id = current_order.id unless use_billing
+      render_next_step @order
+    end
+
+    def load_delivery
+      @order = current_order
+      @order.shipping_method ||= ShippingMethod.order(:price).first
+      @shipping_methods = ShippingMethod.order('price ASC')
+    end
+
+    def load_payment
+      @order = current_order
+      @payment = Forms::PaymentForm.new
+      get_payment_data
+    end
+
+    def process_payment
+      @order = current_order
+      @payment = Forms::PaymentForm.from_params(params[:payment])
+      set_payment_data
+      render_next_step @payment
+    end
+
+    def render_next_step(form)
+      return render_wizard(form) unless form.valid?
+      form.save
+      return redirect_to next_wizard_path unless params[:next_step] == 'confirm'
+      redirect_to wizard_path(:confirm)
     end
 
     def set_payment_data
