fbb5eb32f04e1cf6e430e1e90048022bbca97cf1
b-artem <artemb.dev@gmail.com>
2017-10-30 17:44:41 +0200

    reviews_controller specs

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,14 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
   load_and_authorize_resource :book
   load_and_authorize_resource :review, through: :book
 
   def create
     @review.user = current_user
-    respond_to do |format|
-      if @review.save
-        format.html { redirect_to @review.book,
-                      notice: t('.success') }
-        format.json { render :show, status: :created, location: @review }
-      else
-        format.html { redirect_to @book, flash: { errors: @review.errors } }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
+    if @review.save
+      redirect_to @book, notice: t('.success')
+    else
+      redirect_to @book, flash: { errors: @review.errors }
     end
   end
 
--- app/views/books/_book.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! book, :id, :title, :description, :price, :publication_year, :dimensions, :materials, :created_at, :updated_at
-json.url book_url(book, format: :json)
