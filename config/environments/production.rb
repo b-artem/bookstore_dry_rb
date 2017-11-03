3a69abe43123827b57ce0570e077755e220b495d
b-artem <artemb.dev@gmail.com>
2017-11-03 11:38:01 +0200

    change domain in config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,10 +96,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'example.com',
+    domain:               'bookstore-artem.herokuapp.com',
     user_name:            ENV['GMAIL_USER_NAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true }
-
 end
