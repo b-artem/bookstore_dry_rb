2219a276fc853f3bd2caad3ae31fac9a174796bf
b-artem <artemb.dev@gmail.com>
2020-02-01 12:09:41 +0200

    Setup Docker in progress

---
+++ .dockerdev/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Exit on fail
+set -e
+
+rm -f $APP_HOME/tmp/pids/server.pid
+# rm -f $APP_HOME/tmp/pids/sidekiq.pid
+
+bundle install --jobs $(nproc) --retry 5
+# yarn install --check-files
+
+if [ "$RAILS_ENV" = "staging" ] || [ "$RAILS_ENV" = "production" ]; \
+  then bin/rails assets:precompile; fi
+
+bin/rails db:create
+bin/rails db:migrate
+bin/rails db:seed
+
+exec "$@"
