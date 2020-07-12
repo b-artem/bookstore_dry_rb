5540e2f620af8234eb160f6e55ed243768ef0d62
b-artem <artemb.dev@gmail.com>
2020-07-13 00:14:29 +0300

    Upgrade to Rails 5.2

--- config/application.rb
+++ config/application.rb
@@ -2,17 +2,7 @@
 
 require_relative 'boot'
 
-require 'rails'
-# Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'action_view/railtie'
-require 'action_cable/engine'
-require 'sprockets/railtie'
-require 'fog/aws'
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,20 +11,22 @@ Bundler.require(*Rails.groups)
 module Bookstore
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     config.autoload_paths << 'lib'
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
     # Don't generate system test files.
     config.generators.system_tests = nil
 
     config.generators do |g|
       g.test_framework :rspec
     end
+
     config.i18n.default_locale = :en
+
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
